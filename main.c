aa671b543ffffef5985cd21e8e2672ded854574c
samuelSchulthies <samuelschulthies@gmail.com>
2022-09-09 22:44:14 -0600

    Add initial files

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char toothless = 0;
+    short hiccup = 1;
+    int astrid = 2;
+    long gobber = 3;
+    float fishlegs = 4;
+    double stoic = 5;
+
+    char *toothless_p = &toothless;
+    short *hiccup_p = &hiccup;
+    int *astrid_p = &astrid;
+    long *gobber_p = &gobber;
+    float *fishlegs_p = &fishlegs;
+    double *stoic_p = &stoic;
+
+    printf ("%p\n", toothless_p);
+    printf ("%p\n", hiccup_p);
+    printf ("%p\n", astrid_p);
+    printf ("%p\n", gobber_p);
+    printf ("%p\n", fishlegs_p);
+    printf ("%p\n", stoic_p);
+
+
     return 0;
 }
