dedb6d79b27a384f77e64b1f720fe508625117cd
samuelSchulthies <samuelschulthies@gmail.com>
2022-09-13 12:39:55 -0600

    Add initial files

--- main.c
+++ main.c
@@ -1,47 +1,55 @@
 #include <stdio.h>
 
 int main() {
-    char toothless = 0;
-    short hiccup = 1;
-    int astrid = 2;
-    long gobber = 3;
-    float fishlegs = 4;
-    double stoic = 5;
+//    char toothless = 0;
+//    short hiccup = 1;
+//    int astrid = 2;
+//    long gobber = 3;
+//    float fishlegs = 4;
+//    double stoic = 5;
+//
+//    char b = 'b';
+//    char e = 'e';
+//    char r = 'r';
+//    char k = 'k';
+//
+//    int dragons [3] = {1, 2, 3};
+//    char berk [4] = {b, e, r, k};
+//
+//    int *dragons_p = (int *) &dragons;
+//    char *berk_p = (char *) &berk;
+//
+//
+//
+//    char *toothless_p = &toothless;
+//    short *hiccup_p = &hiccup;
+//    int *astrid_p = &astrid;
+//    long *gobber_p = &gobber;
+//    float *fishlegs_p = &fishlegs;
+//    double *stoic_p = &stoic;
+//
+//    printf ("%p\n", toothless_p);
+//    printf ("%p\n", hiccup_p);
+//    printf ("%p\n", astrid_p);
+//    printf ("%p\n", gobber_p);
+//    printf ("%p\n", fishlegs_p);
+//    printf ("%p\n", stoic_p);
+//
+//    for (int i = 0; i < 3; ++i){
+//        printf ("dragons: %p\n", dragons_p[i]);
+//    }
+//
+//    for (int i = 0; i < 4; ++i){
+//        printf ("berk: %p\n", berk_p[i]);
+//    }
 
-    char b = 'b';
-    char e = 'e';
-    char r = 'r';
-    char k = 'k';
+int userInput [5];
 
-    int dragons [3] = {1, 2, 3};
-    char berk [4] = {b, e, r, k};
+printf ("Give me 5 numbers\n");
 
-    int *dragons_p = (int *) &dragons;
-    char *berk_p = (char *) &berk;
-
-
-
-    char *toothless_p = &toothless;
-    short *hiccup_p = &hiccup;
-    int *astrid_p = &astrid;
-    long *gobber_p = &gobber;
-    float *fishlegs_p = &fishlegs;
-    double *stoic_p = &stoic;
-
-    printf ("%p\n", toothless_p);
-    printf ("%p\n", hiccup_p);
-    printf ("%p\n", astrid_p);
-    printf ("%p\n", gobber_p);
-    printf ("%p\n", fishlegs_p);
-    printf ("%p\n", stoic_p);
-
-    for (int i = 0; i < 3; ++i){
-        printf ("dragons: %p\n", dragons_p[i]);
-    }
-
-    for (int i = 0; i < 4; ++i){
-        printf ("berk: %p\n", berk_p[i]);
-    }
+for (int i = 0; i < 5; ++i){
+    scanf ("%d", &userInput[i]);
+}
 
 
     return 0;
